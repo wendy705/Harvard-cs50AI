1a613e0d987eef682e6b3adb2264256203eba618
wendy705 <62213824+wendy705@users.noreply.github.com>
2021-08-09 22:19:48 +0200

    Add files via upload

--- traffic/traffic.py
+++ traffic/traffic.py
@@ -89,20 +89,23 @@ def get_model():
             32, (3, 3), activation="relu", input_shape=(IMG_WIDTH, IMG_HEIGHT, 3)
         ),
 
-        # Max-pooling layer, using 2x2 pool size
-        tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
+        # max-pooling layer, using 3x3 pool size
+        tf.keras.layers.MaxPooling2D(pool_size=(3, 3)),
 
-        # 2nd convolutional layer. Learn 32 filters using a 3x3 kernel
-        tf.keras.layers.Conv2D(
-            32, (3, 3), activation="relu", input_shape=(IMG_WIDTH, IMG_HEIGHT, 3)
-        ),
+        # # 2nd convolutional layer. Learn 32 filters using a 3x3 kernel
+        # tf.keras.layers.Conv2D(
+        #     32, (3, 3), activation="relu", input_shape=(IMG_WIDTH, IMG_HEIGHT, 3)
+        # ),
 
         # Flatten units
         tf.keras.layers.Flatten(),
 
         # Add a hidden layer with dropout
-        tf.keras.layers.Dense(128, activation="relu"),
+        tf.keras.layers.Dense(NUM_CATEGORIES * 32, activation="relu"),
         tf.keras.layers.Dropout(0.5),
+
+        # Add a hidden layer
+        tf.keras.layers.Dense(NUM_CATEGORIES * 8, activation="relu"),
 
         # Add an output layer with output unit for all categories
         tf.keras.layers.Dense(NUM_CATEGORIES, activation="softmax")
@@ -114,6 +117,7 @@ def get_model():
         loss="categorical_crossentropy",
         metrics=["accuracy"]
     )
+    model.summary()
 
     return model
 
